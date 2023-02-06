1e196b3414c68ea2a966ceb2b4963f6a35821baf
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-02-06 13:12:33 +0200

    set in one file

--- src/axiosing.js
+++ src/axiosing.js
@@ -1,46 +1,46 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const MY_API_KEY = '33373070-0a3de92214998aff69d545527';
-const ENDPOINT = 'https://pixabay.com/api/?key=';
-// const axios = require('axios');
+// const MY_API_KEY = '33373070-0a3de92214998aff69d545527';
+// const ENDPOINT = 'https://pixabay.com/api/?key=';
+// // const axios = require('axios');
 
-class ImgApi {
-  constructor() {
-    this.queryPage = 1;
-    this.searchQuery = '';
-  }
+// class ImgApi {
+//   constructor() {
+//     this.queryPage = 1;
+//     this.searchQuery = '';
+//   }
 
-  async AxioSearch() {
-    return await axios
-      .get(
-        `${ENDPOINT}${MY_API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.queryPage}`
-      )
-      .then(response => {
-        console.log('ImgApi >>> AxioSearch >>> response', response);
-        console.log('response.data.totalHits ', response.data.totalHits);
-        if (response.data.totalHits === 0) {
-          throw new Error(response.statusText);
-        }
+//   async AxioSearch() {
+//     return await axios
+//       .get(
+//         `${ENDPOINT}${MY_API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.queryPage}`
+//       )
+//       .then(response => {
+//         console.log('ImgApi >>> AxioSearch >>> response', response);
+//         console.log('response.data.totalHits ', response.data.totalHits);
+//         if (response.data.totalHits === 0) {
+//           throw new Error(response.statusText);
+//         }
 
-        // console.log(response.data.totalHits);
-        console.log('response.data', response.data);
-        return response.data;
-      })
-      .then(data => {
-        console.log('ImgApi >>> AxioSearch >>> data', data);
-        this.incrementPage();
+//         // console.log(response.data.totalHits);
+//         console.log('response.data', response.data);
+//         return response.data;
+//       })
+//       .then(data => {
+//         console.log('ImgApi >>> AxioSearch >>> data', data);
+//         this.incrementPage();
 
-        return data;
-      });
-  }
+//         return data;
+//       });
+//   }
 
-  resetPage() {
-    this.queryPage = 1;
-  }
+//   resetPage() {
+//     this.queryPage = 1;
+//   }
 
-  incrementPage() {
-    this.queryPage += 1;
-  }
-}
+//   incrementPage() {
+//     this.queryPage += 1;
+//   }
+// }
 
-export { ImgApi };
+// export { ImgApi };
