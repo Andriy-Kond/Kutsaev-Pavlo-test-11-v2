1b988a3dd86c1771646581907eae1981ef2def7f
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-02-06 13:05:55 +0200

    + uncomment css tag in HTML

--- src/axiosing.js
+++ src/axiosing.js
@@ -8,26 +8,7 @@ class ImgApi {
   constructor() {
     this.queryPage = 1;
     this.searchQuery = '';
-    // this.countImg = 0;
   }
-
-  // async AxioSearch() {
-  //     const BASE_URL = 'https://pixabay.com/api/';
-
-  //     const searchParams = new URLSearchParams({
-  //       key: `33373070-0a3de92214998aff69d545527`,
-  //       image_type: 'photo',
-  //       orientation: 'horizontal',
-  //       safesearch: true,
-  //       per_page: 40,
-  //     });
-
-  //     const response = await axios.get(
-  //       `${BASE_URL}?${searchParams}&q=${this.searchQuery}&page=${this.queryPage}`
-  //     );
-
-  //     return response.data;
-  //   }
 
   async AxioSearch() {
     return await axios
@@ -60,10 +41,6 @@ class ImgApi {
   incrementPage() {
     this.queryPage += 1;
   }
-
-  // resetCountImg() {
-  //     this.countImg = 0;
-  //   }
 }
 
 export { ImgApi };
